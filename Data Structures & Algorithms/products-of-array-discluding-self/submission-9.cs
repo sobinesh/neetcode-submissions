b16760c05e8fe27a16e6908a63e1d9a71a53723a
sobinesh <sobinesh@gmail.com>
2026-04-21 15:59:33 +0530

    Add: products-of-array-discluding-self - submission-9

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-9.cs
@@ -0,0 +1,46 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) 
+    {
+        long totalProduct = 1;
+        long zeroTotal = 1;
+        int n = 0;
+
+        foreach(int i in nums)
+        {
+            totalProduct *= i;
+            
+            if(i==0)
+            {
+                n++;
+                continue;
+            }
+
+            zeroTotal *= i;
+        }
+
+        if(n>1)
+           return new int[nums.Length];
+           
+        for(int i=0; i<nums.Length;i++)
+        {
+            int num = nums[i];
+            if(num!=0)
+            {
+                if(totalProduct == 0)
+                {
+                     nums[i]=0;
+                     continue;
+                }
+
+                nums[i] = (int) totalProduct/num; 
+            }
+            else
+            {
+                nums[i] = (int)zeroTotal;
+            }
+        }
+
+        return nums;
+        
+    }
+}
